38e3487f283fa8041e34e15e842a0e975cf28e4b
Arvind R K <52023592+arvindrk47@users.noreply.github.com>
2023-09-21 11:25:33 +0530

    chatroom rooms were added

--- chatroom/main.py
+++ chatroom/main.py
@@ -8,10 +8,69 @@ app = Flask(__name__)
 app.config["SECRET_KEY"]='1e5a3bd7e61ef7ca7e421333af331d2a'
 socketio = SocketIO(app)
 
+
+rooms = {}
+
+def generate_unique_code(Length):
+    while True:
+        code = ""
+        for _ in range(Length):
+            code  += random.choice(ascii_uppercase)
+
+        if code not in rooms:
+            break
+    return code
+
 @app.route('/',methods=["GET","POST"])
-def index():
+def home():
+    session.clear()
+    if request.method == 'POST':
+        name = request.form.get("name")
+        code = request.form.get("code")
+        join = request.form.get("join",False)
+        create = request.form.get("create", False)
+
+        if not name:
+            return render_template("home.html", error="Please enter the name",code=code, name=name)
+        
+        if join != False and not code:
+            return render_template("home.html", error = "Please enter a room code",code=code, name=name)
+        
+        room = code
+        if create != False:
+            room = generate_unique_code(4)
+            rooms[room] = {"members":0, "messages":[]}
+
+        elif code not in rooms:
+            return render_template("home.html", error="Room does not exist", code=code, name=name)
+        
+
+        session["room"] = room
+        session["name"] = name
+        return redirect(url_for("room"))
     return render_template("home.html")
 
+
+@app.route("/room")
+def room():
+    room =  session.get("room")
+    if room is None or session.get("name") is None or room not in rooms:
+        return redirect(url_for("home"))
+    return render_template("room.html")
+
+@socketio.on("connect")
+def connect(auth):
+    room = session.get("room")
+    name = session.get("name")
+    if not room or not name:
+        return
+    if room not in rooms:
+        leave_room(room)
+        return
+    join_room(room)
+    send({"name":name, "messages":"has enter the room"}, to=room)
+    rooms[room]["members"] +=1
+    print(f"{name} joined the {room}")
 
 if __name__=="__main__":
     socketio.run(app, debug=True)
