9853025f16e9cfa03ecf0bb65084befaf5814e6c
Arvind R K <52023592+arvindrk47@users.noreply.github.com>
2023-09-30 15:54:43 +0530

    updated flask-form

--- form-app/main.py
+++ form-app/main.py
@@ -1,31 +1,28 @@
-from flask import Flask, render_template, request
-
-
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-details =[]
 
-@app.route("/", methods=["GET", "POST"])
-def home():
-    if request.method=="POST":
-        print(request.form)
-        details.append(
-            (
-                request.form.get("firstname"),
-                request.form.get("phone"),
-                request.form.get("email")
-            )
-        )
-    return render_template("form.html")
+@app.route('/')
+def index():
+    return render_template('form.html')
 
+@app.route('/register', methods=['POST'])
+def register():
+    if request.method == 'POST':
+       
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
 
-@app.route("/result")
-def result():
-    return render_template("result.html",email=email)
+        
+        result_data = {
+            'First Name': first_name,
+            'Last Name': last_name,
+            'Email': email,
+        }
 
-if __name__=="__main__":
+        
+        return render_template('result.html', result=result_data)
+
+if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
